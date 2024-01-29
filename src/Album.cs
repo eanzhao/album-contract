3c6d8316090cfce053b23389834b0b2660828b04
eanzhao <yiqi.zhao@aelf.io>
2024-01-29 12:17:30 +0800

    Add Initailize method

--- src/Album.cs
+++ src/Album.cs
@@ -3,28 +3,26 @@ using Google.Protobuf.WellKnownTypes;
 
 namespace AElf.Contracts.Album
 {
-    // Contract class must inherit the base class generated from the proto file
     public class Album : AlbumContainer.AlbumBase
     {
-        // A method that modifies the contract state
+        public override Empty Initialize(Empty input)
+        {
+            return new Empty();
+        }
+
         public override Empty Update(StringValue input)
         {
-            // Set the message value in the contract state
             State.Message.Value = input.Value;
-            // Emit an event to notify listeners about something happened during the execution of this method
-            Context.Fire(new UpdatedMessage
+            Context.Fire(new MessageUpdated
             {
                 Value = input.Value
             });
             return new Empty();
         }
 
-        // A method that read the contract state
         public override StringValue Read(Empty input)
         {
-            // Retrieve the value from the state
             var value = State.Message.Value;
-            // Wrap the value in the return type
             return new StringValue
             {
                 Value = value
